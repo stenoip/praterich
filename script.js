48b9376e41675e6d5236c20bb94e69b25b47ff83
Stenoip Company <mystenoipcompany@gmail.com>
2025-10-21 10:40:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
     // Custom Pronunciations for Text-to-Speech (TTS)
     var customPronunciations = {
       "Praterich": "Prah-ter-rich",
-      "Stenoip": "Stick-noh-ip"
+      "Stenoip": "Stick-no-ikp"
     };
 
     // Praterich A.I. Core Personality Profile (Front-end System Instruction)
